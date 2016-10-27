dc5c9d8d625a4dc8bcc7f06efc0e1b721055d6a6
luanpn <hanifo159@gmail.com>
2016-10-27 16:52:54 -0300

    Add files via upload
    First Commit of the project Smart Greenhouse, contains the first version of the project , explaining about the sensors and actuators.

---
+++ smartGreenHouse_Atuation.ino
@@ -0,0 +1,55 @@
+#include <Arduino.h>
+/*
+Codigo do No atuador
+
+*/
+
+
+int lamp1 = 1;
+int lamp2 = 2;
+int fan = 3;
+int regador = 4;
+
+
+void setup() {
+  pinMode(lamp1, OUTPUT);
+  pinMode(lamp2, OUTPUT);
+  pinMode(fan, OUTPUT);
+  pinMode(regador, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+//codigo para receber informacoes do Servidor Mqtt e possivelmente fazer alguma atuacao.
+
+}
+
+
+//metodos para ligar ou desligar os atuadores, os sinais sao enviados para um Rele para realizar os procedimentos.
+void ligarLampada1(){
+  digitalWrite(lamp1,HIGH);
+  }
+void desligarLampada1(){
+  digitalWrite(lamp1,LOW);
+  }
+
+void ligarLampada2(){
+  digitalWrite(lamp2,HIGH);}
+void desligarLampada2(){
+  digitalWrite(lamp2,LOW);
+  }
+
+void ligarFAN(){
+  digitalWrite(fan,HIGH);
+  }
+void desligarFAN(){
+  digitalWrite(fan,LOW);
+  }
+
+void LigarRegador(){
+  digitalWrite(regador,HIGH);
+  }
+void DesligarRegador(){
+  digitalWrite(regador,LOW);
+  }
---
+++ smartGreenHouse_Sensors.ino
@@ -0,0 +1,59 @@
+#include <Arduino.h>
+//funcao para converter o valor para lumens
+double Light (int RawADC0){
+double Vout=RawADC0*0.0048828125;
+int lux=(2500/Vout-500)/10;
+return lux;
+}
+
+//funcao para converter a temperatura para celcius
+double temperature(int RawADC0){
+  double Vout=RawADC0*0.0048828125;
+  double tem=(Vout-0.5)*100;
+  return tem;
+  }
+
+int umidaSensor = A2;
+
+
+float luminValue = 0;
+float tempValue = 0;
+float umidaValue = 0;
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+
+  //inicializacao dos valores dos sensores
+  luminValue =(Light(analogRead(0)));
+  tempValue = (temperature(analogRead(1)));
+  //umidaValue = analogRead(umidaSensor);
+
+
+
+//converte a umidade do solo
+  //falta ter nocao dos retornos do sensor e das unidades de medicao de umidade
+
+  Serial.println("------------");
+  Serial.println(luminValue);
+  
+  Serial.println(tempValue);
+ 
+  //Serial.println(umidaValue);
+
+  /*
+  neste ponto do codigo os valores ja foram lidos e convertidos para as dimensoens proprias para analize segundo a literartura
+  o sensoriamento deste NO foi concluido restando agora o envio para o servidor MQTT.
+  */
+
+  //inicio do bloco de envio
+
+
+
+
+
+}
